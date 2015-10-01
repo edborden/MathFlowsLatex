663eef64cb9ee0d24b83cd398f82d9b5a181a6c6
edborden <borden.edward@gmail.com>
2015-10-01 16:16:01 -0400

    mime type svg

--- app/controllers/render_controller.rb
+++ app/controllers/render_controller.rb
@@ -2,7 +2,7 @@ class RenderController < ApplicationController
 
 	def render
 
-		options = {type: :svg,disposition: "attachment",filename: "equation.svg"}
+		options = {type: "image/svg+xml",disposition: "attachment",filename: "equation.svg"}
 		send_file_headers!(options)
 		render options.slice(:status, :content_type).merge(:text => rendered[:data])
 		#send_data rendered[:data]

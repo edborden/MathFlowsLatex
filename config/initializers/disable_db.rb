ac60a699aa3f3ee931398173ea19e4deb555d5df
edborden <borden.edward@gmail.com>
2015-10-03 09:54:57 -0400

    remove db

--- config/initializers/disable_db.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.middleware.tap do |middleware|
-  middleware.delete ActiveRecord::Migration::CheckPending
-  middleware.delete ActiveRecord::ConnectionAdapters::ConnectionManagement
-  middleware.delete ActiveRecord::QueryCache
-end
